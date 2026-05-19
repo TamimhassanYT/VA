e6cb3a1fbe1ceebcabff6c1baec8a3eaf7aa3d62
Tamim_GamingXD <mdtamimhassan62@gmail.com>
2026-05-19 17:15:52 +0600

    Update version number in update data

--- functions/update/latest.js
+++ functions/update/latest.js
@@ -1,7 +1,7 @@
 export async function onRequestPost(context) {
     // Your plugin update data
     const updateData = {
-        version: "1.0.1",
+        version: "1.0.2-1.0.6",
         download_url: "https://voxeladder.zanity.net"
     };
 
